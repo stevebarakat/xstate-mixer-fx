15d9052c8017c11d1a5588a42542a9f87d14fde4
stevebarakat <stevebarakat@gmail.com>
2023-04-27 21:09:32 -0400

    bus button

--- src/components/ChannelStrip.jsx
+++ src/components/ChannelStrip.jsx
@@ -11,6 +11,12 @@ function ChannelStrip({ track, trackIndex, channel }) {
         <option value="reverb">Reverb</option>
         <option value="delay">Delay</option>
       </select>
+      <div className="bus-btn">
+        <input id={`bus1${trackIndex}`} type="checkbox" />
+        <label htmlFor={`bus1${trackIndex}`}>1</label>
+        <input id={`bus2${trackIndex}`} type="checkbox" />
+        <label htmlFor={`bus2${trackIndex}`}>2</label>
+      </div>
       <div className="channel">
         <div className="chan-strip-btn">
           <Solo trackIndex={trackIndex} channel={channel} />
