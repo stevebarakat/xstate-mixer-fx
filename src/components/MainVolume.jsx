13ec9298c3f1ecf9f5162d868ca7a2a0f6b5ad16
stevebarakat <stevebarakat@gmail.com>
2023-05-07 15:46:33 -0400

    main volume

--- src/components/MainVolume.jsx
+++ src/components/MainVolume.jsx
@@ -1,28 +1,25 @@
+import { useState } from "react";
 import { Destination } from "tone";
-import { MixerMachineContext } from "../App";
+import { dBToPercent, scale } from "../utils/scale";
 import Range from "./Range";
 
 function MainVolume({ channel, trackIndex }) {
-  const [state, send] = MixerMachineContext.useActor();
-
+  const [mainVolume, setMainVolume] = useState(-32);
   return (
     <div className="channel">
-      <div className="window">
-        {`${state.context.mainVolume.toFixed(0)} dB`}
-      </div>
+      <div className="window">{`${mainVolume.toFixed(0)} dB`}</div>
       <div className="range-y">
         <Range
           id="main"
           min={-100}
           max={12}
           step={0.1}
-          value={state.context.mainVolume}
+          value={mainVolume}
           onChange={(e) => {
-            send({
-              type: "CHANGE_MAIN_VOLUME",
-              target: e.target,
-              channel: Destination,
-            });
+            const value = parseFloat(e.target.value);
+            const scaled = dBToPercent(scale(value));
+            Destination.volume.value = scaled;
+            setMainVolume(value);
           }}
         />
       </div>
